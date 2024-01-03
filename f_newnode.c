50a663062c6192144a0283a2fcea637e3811ec75
Nacho <146840051+rivero04@users.noreply.github.com>
2024-01-03 16:41:30 -0300

    task monty

--- f_newnode.c
+++ f_newnode.c
@@ -1,27 +1,28 @@
 #include "monty.h"
 /**
- * addnode - add node to the head stack
- * @head: head of the stack
- * @i: new_value
- * Return: no return
- */
+* addnode - add node to top of stack
+* @head: first node
+* @n: new value
+*
+* Return: none
+*/
 void addnode(stack_t **head, int n)
 {
-	stack_t *aux, *newnode = malloc(sizeof(stack_t));
-	
-	if (newnode == NULL)
+	stack_t *new_node, *temp;
+
+	temp = *head;
+	new_node = malloc(sizeof(stack_t));
+
+	if (new_node == NULL)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
+		printf("Error\n");
+		exit(0);
 	}
+	if (temp)
+		temp->prev = new_node;
 
-    aux = *head;
-    
-    if (aux)
-        aux->prev = newnode;
-
-    newnode->n = n;
-	newnode->next = *head;
-    newnode->prev = NULL;
-	*head = newnode;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
 }
